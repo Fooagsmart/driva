5194adea843fee791d8299cb141422341f93ed77
oluwaseun <adunmofestus@gmail.com>
2024-01-30 04:33:33 +0100

    testing useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count , setCount] = useState(0)
+
+  const increase = () => {
+    setCount(count + 1);
+  }
+
+  const decrease = () => {
+    setCount(count -1)
+  }
+
+  const reset = () => {
+    setCount(0)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increase}>increase</button>
+      <button onClick={decrease}>decrease</button>
+      <button onClick={reset}>default</button>
+
+      {count}
     </div>
   );
 }
