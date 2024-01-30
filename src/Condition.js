38e29bbae4561f0dbfede07bf3fbd449f70e2239
oluwaseun <adunmofestus@gmail.com>
2024-01-30 04:54:33 +0100

    mapping

--- src/Condition.js
+++ src/Condition.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Condition = () => {
+  const planets = [
+    { name: "Mars", isGasPlanet: false },
+    { name: "Earth", isGasPlanet: false },
+    { name: "Jupiter", isGasPlanet: true },
+    { name: "Venus", isGasPlanet: false },
+    { name: "Neptune", isGasPlanet: true },
+    { name: "Uranus", isGasPlanet: true },
+  ];
+
+  const [showGasPlanets, setShowGasPlanets] = useState(false);
+
+  const filteredPlanets = planets.filter((planet) => planet.isGasPlanet === showGasPlanets);
+
   return (
     <div>
-      work
+      <label>
+        <input type="checkbox" onChange={() => setShowGasPlanets(!showGasPlanets)} />
+        Show Gas Planets
+      </label>
+      <ul>
+        {filteredPlanets.map((planet) => (
+          <li key={planet.name}>{planet.name}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Condition
+export default Condition;
