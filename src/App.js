bb070fd9026e3bfe4d2d97c2da23fe6c1c1bb8f9
oluwaseun <adunmofestus@gmail.com>
2024-02-02 21:42:48 +0100

    refresh

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import { useState } from 'react';
 import './App.css';
-import Condition from './Condition';
-import Taskbar from './Taskbar';
+
 
 function App() {
-
-  const [count , setCount] = useState(0)
-
-  const increase = () => {
-    setCount(count + 1);
-  }
-
-  const decrease = () => {
-    setCount(count -1)
-  }
-
-  const reset = () => {
-    setCount(0)
-  }
   return (
     <div className="App">
-      <button onClick={increase}>increase</button>
-      <button onClick={decrease}>decrease</button>
-      <button onClick={reset}>default</button>
-
-      {count}
-      < Condition />
-      <Taskbar />
+   
     </div>
   );
 }
