6ae9332122522c6b42ed69ab070e93467eb07cdc
oluwaseun <adunmofestus@gmail.com>
2024-02-04 23:31:57 +0100

    wants to start building

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
-import Condition from './Condition';
-import Menu from './Menu';
-import Home from './Home';
-import Navbar from './Navbar';
-import Profile from './Profile';
-import { useState, createContext } from 'react';
+import React from 'react'
 
-export const AppContext = createContext();
-
-function App() {
-  const [username, setUsername] = useState("Developer Ayo");
+const App = () => {
   return (
-    <div className="App">
-      <AppContext.Provider value={{username, setUsername}}>
-      <Router >
-        <Navbar />
-        <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/Menu' element={<Menu />}></Route>
-        <Route path='/Condition' element={<Condition />}></Route>
-        <Route path='/profile' element={<Profile />}></Route>
-        <Route path='*' element={<h1>Page not Found</h1>}></Route>
-        </Routes>
-      </Router>
-      </AppContext.Provider>
- 
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
