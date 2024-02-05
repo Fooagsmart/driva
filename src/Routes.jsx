2df8edfebf8e92e78e2eabc405afcc2736937e87
oluwaseun <adunmofestus@gmail.com>
2024-02-05 22:47:40 +0100

    REMODIFIED

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import NotFound from './Pages/NotFound';
 
 const Routes = () => {
   return (
