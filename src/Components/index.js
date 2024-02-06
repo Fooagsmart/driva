f0fe29f88b9b28f2f6cea37d11dd1b6b8289e60c
oluwaseun <adunmofestus@gmail.com>
2024-02-06 03:11:26 +0100

    homepage

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,8 +1,8 @@
 export { Button } from "./Button";
 export { Img } from "./Img";
-
-export { Line } from "./Line"
-export { List } from "./List"
-export { SelectBox } from "./SelectBox"
-export { ReactTable } from "./Table"
-export { Text } from "./Text"
+export { Input } from "./Input";
+export { Line } from "./Line";
+export { List } from "./List";
+export { SelectBox } from "./SelectBox";
+export { ReactTable } from "./Table";
+export { Text } from "./Text";
