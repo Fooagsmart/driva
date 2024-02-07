df930fb6bb77ca1c5b29bb78b00ccbe9d36f620f
oluwaseun <adunmofestus@gmail.com>
2024-02-07 11:48:27 +0100

    error plenty

--- src/Pages/DashBoardHome.jsx
+++ src/Pages/DashBoardHome.jsx
@@ -1,67 +1,14 @@
 import React from "react";
 
-import { Sidebar } from "react-pro-sidebar";
-import { useNavigate } from "react-router-dom";
-
 import { Button, Img, Line, List, Text } from "../Components";
+import Sidebar from "../components/Sidebar";
 
 const DashboardHomePage = () => {
-  const navigate = useNavigate();
-
   return (
     <>
       <div className="bg-gray-50 flex flex-col font-zonapro items-center justify-start mx-auto w-full">
         <div className="flex md:flex-col flex-row md:gap-5 items-start justify-evenly w-full">
-          <Sidebar className="!sticky !w-[214px] bg-white-A700 border-gray-200 border-r border-solid flex h-screen md:hidden justify-start overflow-auto md:px-5 top-[0]">
-            <div className="bg-white-A700 border-b border-gray-200 border-solid flex flex-col items-start justify-start sm:px-5 px-7 py-3.5 w-[214px]">
-              <div className="flex flex-row gap-1 items-center justify-start w-auto">
-                <Img className="h-5 w-5" src="images/img_car.svg" alt="car" />
-                <Text
-                  className="text-blue_gray-900 text-xl tracking-[-0.60px] w-auto"
-                  size="txtZonaProBold20"
-                >
-                  Driva
-                </Text>
-              </div>
-            </div>
-            <div className="flex flex-col font-inter gap-1 items-start justify-start mb-[776px] mt-[18px] w-[214px]">
-              <div className="bg-yellow-900_19 border-l-[3px] border-solid border-yellow-900 flex flex-row gap-2 items-center justify-start px-4 py-2 w-full">
-                <Img className="h-5 w-5" src="images/img_home.svg" alt="home" />
-                <Text
-                  className="text-sm text-yellow-900 w-auto"
-                  size="txtInterMedium14Yellow900"
-                >
-                  Home
-                </Text>
-              </div>
-              <div className="bg-white-A700 flex flex-row gap-2 items-center justify-start px-4 py-2 w-full">
-                <Img
-                  className="h-5 w-5"
-                  src="images/img_thumbsup.svg"
-                  alt="thumbsup"
-                />
-                <Text
-                  className="text-blue_gray-900 text-sm w-auto"
-                  size="txtInterRegular14Bluegray900"
-                >
-                  Payments
-                </Text>
-              </div>
-              <div className="bg-white-A700 flex flex-row gap-2 items-center justify-start px-4 py-2 w-full">
-                <Img
-                  className="h-5 w-5"
-                  src="images/img_profile.svg"
-                  alt="profile"
-                />
-                <Text
-                  className="text-blue_gray-900 text-sm w-auto"
-                  size="txtInterRegular14Bluegray900"
-                >
-                  Profile
-                </Text>
-              </div>
-            </div>
-          </Sidebar>
+          <Sidebar1 className="!sticky !w-[214px] bg-white-A700 border-gray-200 border-r border-solid flex h-screen md:hidden justify-start overflow-auto md:px-5 top-[0]" />
           <div className="flex flex-1 flex-col font-inter gap-5 items-center justify-start md:px-5 w-full">
             <div className="flex flex-col gap-5 justify-start w-full">
               <div className="bg-white-A700 border-b border-gray-300 border-solid flex md:flex-col flex-row md:gap-5 items-center justify-start p-[7px] w-full">
@@ -82,15 +29,8 @@ const DashboardHomePage = () => {
                 />
               </div>
               <Button
-                className="common-pointer border border-gray-200 border-solid cursor-pointer flex items-center justify-center min-w-[96px] ml-10 md:ml-[0] mr-[1378px] rounded"
-                onClick={() => navigate("/dashboardhomethree")}
-                leftIcon={
-                  <Img
-                    className="h-5 mr-1"
-                    src="images/img_arrow_left_blue_gray_900.svg"
-                    alt="Arrow - Left"
-                  />
-                }
+                className="border border-gray-200 border-solid cursor-pointer flex items-center justify-center min-w-[96px] ml-10 md:ml-[0] mr-[1378px] rounded"
+                leftIcon={<Img className="h-5 mr-1" src="images/img_arrow_left_blue_gray_900.svg" alt="Arrow - Left" />}
                 shape="round"
                 color="white_A700"
                 size="sm"
@@ -103,23 +43,18 @@ const DashboardHomePage = () => {
               <div className="bg-white-A700 flex flex-col gap-1 h-[62px] md:h-auto items-start justify-start pt-5 px-5 w-full">
                 <div className="flex sm:flex-col flex-row gap-4 items-center justify-start w-full">
                   <Button
-                    className="common-pointer bg-transparent cursor-pointer font-medium min-w-[156px] text-blue_gray-500 text-center text-sm"
-                    onClick={() => navigate("/dashboardhomefour")}
+                    className="bg-transparent cursor-pointer font-medium min-w-[156px] text-blue_gray-500 text-center text-sm"
                     size="md"
                   >
                     Application Information
                   </Button>
                   <Button
-                    className="common-pointer bg-transparent cursor-pointer font-medium min-w-[140px] text-blue_gray-500 text-center text-sm"
-                    onClick={() => navigate("/dashboardhometwo")}
+                    className="bg-transparent cursor-pointer font-medium min-w-[140px] text-blue_gray-500 text-center text-sm"
                     size="md"
                   >
                     Check-up Schedules
                   </Button>
-                  <Text
-                    className="border-b-4 border-blue_gray-900 border-solid pb-[18px] pt-[3px] text-base text-blue_gray-900 w-auto"
-                    size="txtInterSemiBold16"
-                  >
+                  <Text className="border-b-4 border-blue_gray-900 border-solid font-semibold pb-[18px] pt-[3px] text-base text-blue_gray-900 w-auto">
                     Payment Transactions
                   </Text>
                 </div>
@@ -134,70 +69,35 @@ const DashboardHomePage = () => {
                       alt="rectangleOne_One"
                     />
                     <div className="border-b border-gray-200 border-solid flex flex-1 flex-col gap-1 h-full items-start justify-center px-3 w-full">
-                      <Text
-                        className="text-base text-blue_gray-900 w-auto"
-                        size="txtInterSemiBold16"
-                      >
-                        John Anthony
-                      </Text>
-                      <Text
-                        className="text-blue_gray-500 text-sm w-auto"
-                        size="txtInterMedium14Bluegray500"
-                      >
-                        johnanthony01@gmail.com
-                      </Text>
+                      <Text className="font-semibold text-base text-blue_gray-900 w-auto">John Anthony</Text>
+                      <Text className="font-medium text-blue_gray-500 text-sm w-auto">johnanthony01@gmail.com</Text>
                     </div>
                   </div>
                 </div>
                 <div className="bg-white-A700 border border-gray-200 border-solid flex flex-col h-[499px] md:h-auto items-start justify-start max-w-[1434px] py-5 rounded-lg w-full">
                   <div className="flex flex-col items-start justify-start px-5 w-full">
-                    <List
-                      className="flex flex-col gap-4 items-start w-full"
-                      orientation="vertical"
-                    >
+                    <List className="flex flex-col gap-4 items-start w-full" orientation="vertical">
                       <div className="flex flex-1 flex-row gap-3 items-center justify-start my-0 w-full">
-                        <Text
-                          className="text-base text-blue_gray-900 w-auto"
-                          size="txtInterSemiBold16"
-                        >
-                          Recent Transactions
-                        </Text>
+                        <Text className="font-semibold text-base text-blue_gray-900 w-auto">Recent Transactions</Text>
                         <Button
                           className="border border-gray-200 border-solid cursor-pointer flex items-center justify-center min-w-[76px]"
-                          leftIcon={
-                            <Img
-                              className="h-5 mr-1"
-                              src="images/img_calendar.svg"
-                              alt="Calendar"
-                            />
-                          }
+                          leftIcon={<Img className="h-5 mr-1" src="images/img_calendar.svg" alt="Calendar" />}
                           shape="round"
                           color="gray_100"
                           size="sm"
                           variant="fill"
                         >
-                          <div className="font-semibold text-left text-sm">
-                            2024
-                          </div>
+                          <div className="!text-blue_gray-900 font-semibold text-left text-sm">2024</div>
                         </Button>
                       </div>
                       <div className="bg-gray-50 border border-gray-200 border-solid flex flex-1 flex-col items-start justify-start my-0 rounded-lg w-full">
                         <div className="flex md:flex-col flex-row gap-3 items-center justify-start px-5 py-2 rounded-lg w-full">
                           <div className="flex flex-1 flex-col items-center justify-start w-full">
                             <div className="flex flex-col items-start justify-center w-full">
-                              <Text
-                                className="text-base text-blue_gray-900 w-auto"
-                                size="txtInterSemiBold16"
-                              >
-                                November
-                              </Text>
+                              <Text className="font-semibold text-base text-blue_gray-900 w-auto">November</Text>
                             </div>
                           </div>
-                          <Img
-                            className="h-5 w-5"
-                            src="images/img_arrowright.svg"
-                            alt="arrowright"
-                          />
+                          <Img className="h-5 w-5" src="images/img_arrowright.svg" alt="arrowright" />
                         </div>
                       </div>
                       <div className="flex flex-1 flex-col items-center justify-start my-0 w-full">
@@ -205,19 +105,10 @@ const DashboardHomePage = () => {
                           <div className="flex md:flex-col flex-row gap-3 items-center justify-start px-5 py-2 rounded-lg w-full">
                             <div className="flex flex-1 flex-col items-center justify-start w-full">
                               <div className="flex flex-col items-start justify-center w-full">
-                                <Text
-                                  className="text-base text-blue_gray-900 w-auto"
-                                  size="txtInterSemiBold16"
-                                >
-                                  December
-                                </Text>
+                                <Text className="font-semibold text-base text-blue_gray-900 w-auto">December</Text>
                               </div>
                             </div>
-                            <Img
-                              className="h-5 w-5"
-                              src="images/img_arrowright.svg"
-                              alt="arrowright"
-                            />
+                            <Img className="h-5 w-5" src="images/img_arrowright.svg" alt="arrowright" />
                           </div>
                         </div>
                       </div>
