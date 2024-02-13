5c128fdf822eda4a51bf8c0df35eaefd56291c3c
oluwaseun <adunmofestus@gmail.com>
2024-02-13 20:32:11 +0100

    routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,41 +2,41 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "pages/Home";
 import NotFound from "pages/NotFound";
-const DashboardHome = React.lazy(() => import("pages/DashboardHome"));
-const DashboardHomeTwo = React.lazy(() => import("pages/DashboardHomeTwo"));
-const DashboardHomeFour = React.lazy(() => import("pages/DashboardHomeFour"));
-const DashboardHomeThree = React.lazy(() => import("pages/DashboardHomeThree"));
-const DashboardProfile = React.lazy(() => import("pages/DashboardProfile"));
+const DashboardHome = React.lazy(() => import("./Pages/DashBoardHome"));
+const DashboardHomeTwo = React.lazy(() => import("./pages/DashboardHomeTwo"));
+const DashboardHomeFour = React.lazy(() => import("./pages/DashboardHomeFour"));
+const DashboardHomeThree = React.lazy(() => import("./pages/DashboardHomeThree"));
+const DashboardProfile = React.lazy(() => import("./pages/DashboardProfile"));
 const DashboardProfileOne = React.lazy(
-  () => import("pages/DashboardProfileOne"),
+  () => import("./pages/DashboardProfileOne"),
 );
 const DashboardPaymentsOne1 = React.lazy(
-  () => import("pages/DashboardPaymentsOne1"),
+  () => import("./pages/DashboardPaymentsOne1"),
 );
 const DashboardPaymentsOne = React.lazy(
-  () => import("pages/DashboardPaymentsOne"),
+  () => import("./pages/DashboardPaymentsOne"),
 );
-const DashboardPayments = React.lazy(() => import("pages/DashboardPayments"));
-const DashboardHomeOne = React.lazy(() => import("pages/DashboardHomeOne"));
+const DashboardPayments = React.lazy(() => import("./pages/DashboardPayments"));
+const DashboardHomeOne = React.lazy(() => import("./pages/DashboardHomeOne"));
 const Desktop1ChangePassword = React.lazy(
-  () => import("pages/Desktop1ChangePassword"),
+  () => import("./pages/Desktop1ChangePassword"),
 );
-const Desktop5Success = React.lazy(() => import("pages/Desktop5Success"));
-const Desktop5Loading = React.lazy(() => import("pages/Desktop5Loading"));
+const Desktop5Success = React.lazy(() => import("./pages/Desktop5Success"));
+const Desktop5Loading = React.lazy(() => import("./pages/Desktop5Loading"));
 const Desktop5Confirmation = React.lazy(
-  () => import("pages/Desktop5Confirmation"),
+  () => import("./pages/Desktop5Confirmation"),
 );
 const Desktop4DocumentUpload = React.lazy(
-  () => import("pages/Desktop4DocumentUpload"),
+  () => import("./pages/Desktop4DocumentUpload"),
 );
 const Desktop3GuarantorInformation = React.lazy(
-  () => import("pages/Desktop3GuarantorInformation"),
+  () => import("./pages/Desktop3GuarantorInformation"),
 );
 const Desktop2VehicleInformation = React.lazy(
-  () => import("pages/Desktop2VehicleInformation"),
+  () => import("./pages/Desktop2VehicleInformation"),
 );
 const Desktop1PersonalInformation = React.lazy(
-  () => import("pages/Desktop1PersonalInformation"),
+  () => import("./Pages/Desktop1PersonalInformation"),
 );
 const ProjectRoutes = () => {
   return (
